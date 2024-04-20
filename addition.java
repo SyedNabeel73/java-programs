8a7551524aa0b3af0c0b3641ba31b0fff1ae95d9
SyedNabeel73 <149360153+SyedNabeel73@users.noreply.github.com>
2024-04-20 15:40:03 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+package arthimeticop;
+public class addition{
+    private int d1,d2;
+    public addition(int n1,int n2){
+        this.d1=n1;
+        this.d2=n2;}
+        public void sum()
+        {
+            System.out.println("sum of two numbers is"+(d1+d2));
+        }
+    }
